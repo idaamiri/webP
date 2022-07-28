850900e1b465cc98ce48fe33bef6feaaeff7604c
idaamiri <iamidaamiri80@gmail.com>
2022-07-29 00:39:08 +0200

    loops

--- js1/index.js
+++ js1/index.js
@@ -13,3 +13,43 @@ if (newZ == "Australia" || "Germany") {
   }
 }
 console.log(newC);
+
+let myNewText = "";
+let i = 0;
+while (i < 10) {
+  myNewText += "The number is " + i + " ";
+  i++;
+}
+console.log(myNewText);
+
+let myNewText1 = "";
+for (let o = 0; o < 10; o++) {
+  myNewText1 += "The number is " + o + " ";
+}
+console.log(myNewText1);
+
+let myNewText2 = "";
+let f = 0;
+do {
+  myNewText2 += "Student number : " + f + "";
+  f++;
+} while (f < 5);
+console.log(myNewText2);
+
+let myNewText3 = "";
+for (let u = 0; u < 10; u++) {
+  if (u == 6) {
+    break;
+  }
+  myNewText3 += "The number is " + u + " ";
+}
+console.log(myNewText3);
+
+let myNewText4 = "";
+for (let x = 0; x < 6; x++) {
+  if (x == 3) {
+    continue;
+  }
+  myNewText4 += "The number is " + x + " ";
+}
+console.log(myNewText4);
