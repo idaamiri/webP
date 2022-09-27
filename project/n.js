9fae5151d78f7031a70f1b5f0196b203a2937d42
idaamiri <iamidaamiri80@gmail.com>
2022-09-28 01:42:53 +0200

    js practicing

--- project/n.js
+++ project/n.js
@@ -1,1 +1,26 @@
-let imgEl = document.getElementById("img-el");
+const btn = document.querySelector("#myBtn");
+const textBtn = document.querySelector("#line");
+
+btn.addEventListener("click", function () {
+  textBtn.textContent = "hello";
+});
+
+const myBtn = document.querySelector("#myNewBtn");
+const p1 = document.querySelector("#paragraph");
+
+myBtn.addEventListener("click", myFunction1);
+myBtn.addEventListener("click", myFunction2);
+
+function myFunction1() {
+  p1.textContent += " First Error";
+}
+function myFunction2() {
+  p1.textContent += " Second Error";
+}
+
+myBtn.addEventListener("mouseover", function () {
+  p1.textContent += "mouse overed!";
+});
+myBtn.addEventListener("mouseout", function () {
+  p1.textContent += "mouse out!";
+});
